e341f82d9a00053003c1b0b0d0df450ed8a6395d
Chirag Anand <113680121+chiraganand05@users.noreply.github.com>
2024-09-22 11:57:00 +0530

    Create Operators.sql

---
+++ Operators.sql
@@ -0,0 +1,12 @@
+SELECT (5+2) AS Addition
+SELECT (5-2) AS substraction
+SELECT (5*2) AS multiply
+SELECT (5/2) AS divide
+SELECT (5%2) AS modulo
+
+SELECT 20=50 AS equal_operator
+SELECT 20!=40 AS notequal_operator
+SELECT 210>=50 AS greaterthanequal_operator
+SELECT 14<=80 AS lessthanequal_operator
+SELECT 40>17 AS greaterthan_operator
+SELECT 35<14 AS lessthan_operator
